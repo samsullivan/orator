ea86d28e200645fc0018f180726f42b8d01d5ee4
Sam Sullivan <samsullivan09@gmail.com>
2014-10-27 16:22:57 -0500

    Add custom toolbar

--- app/src/main/java/com/samsullivan/orator/ui/BaseActivity.java
+++ app/src/main/java/com/samsullivan/orator/ui/BaseActivity.java
@@ -2,13 +2,33 @@ package com.samsullivan.orator.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.samsullivan.orator.R;
 
 public class BaseActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void setContentView(int layoutID) {
+        super.setContentView(layoutID);
+        getToolbar();
+    }
+
+    public Toolbar getToolbar() {
+        if(this.toolbar == null) {
+            this.toolbar = (Toolbar) findViewById(R.id.layout_toolbar);
+            setSupportActionBar(this.toolbar );
+        }
+
+        return this.toolbar ;
     }
 
 }
--- app/src/main/java/com/samsullivan/orator/ui/HomeActivity.java
+++ app/src/main/java/com/samsullivan/orator/ui/HomeActivity.java
@@ -1,13 +1,19 @@
 package com.samsullivan.orator.ui;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.samsullivan.orator.R;
 
 public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.home_layout);
 
+        Toolbar toolbar = this.getToolbar();
+        toolbar.setTitle(R.string.app_name);
     }
 
 }
