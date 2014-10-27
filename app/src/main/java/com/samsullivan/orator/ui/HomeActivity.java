e1cffdf37a1ff0b5a27f22ae7a0cefa4e49be06e
Sam Sullivan <samsullivan09@gmail.com>
2014-10-27 15:50:31 -0500

    Initial commit

---
+++ app/src/main/java/com/samsullivan/orator/ui/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.samsullivan.orator.ui;
+
+import android.os.Bundle;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
