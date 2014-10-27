3bc20106b78682f47a261ae615ccca2e34c8ead4
Sam Sullivan <samsullivan09@gmail.com>
2014-10-27 17:43:29 -0500

    Add settings activity

--- app/src/main/java/com/samsullivan/orator/ui/BaseActivity.java
+++ app/src/main/java/com/samsullivan/orator/ui/BaseActivity.java
@@ -1,34 +1,49 @@
 package com.samsullivan.orator.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.samsullivan.orator.R;
 
 public class BaseActivity extends ActionBarActivity {
 
-    private Toolbar toolbar;
+    protected String pageTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        this.pageTitle = getString(R.string.app_name);
     }
 
     @Override
     public void setContentView(int layoutID) {
         super.setContentView(layoutID);
-        getToolbar();
+        createToolbar();
     }
 
-    public Toolbar getToolbar() {
-        if(this.toolbar == null) {
-            this.toolbar = (Toolbar) findViewById(R.id.layout_toolbar);
-            setSupportActionBar(this.toolbar );
-        }
+    public void createToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.layout_toolbar);
 
-        return this.toolbar ;
+        toolbar.setTitle(this.pageTitle);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void openSettings(MenuItem item) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/samsullivan/orator/ui/HomeActivity.java
+++ app/src/main/java/com/samsullivan/orator/ui/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.samsullivan.orator.ui;
 
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.samsullivan.orator.R;
 
@@ -10,10 +9,8 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.home_layout);
 
-        Toolbar toolbar = this.getToolbar();
-        toolbar.setTitle(R.string.app_name);
+        super.setContentView(R.layout.home_layout);
     }
 
 }
